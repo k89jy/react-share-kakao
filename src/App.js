b5b6d2ac5daf27b151990bbcbd264408004f3801
k89jy <k89jy@naver.com>
2021-09-29 19:19:57 +0900

    feat: initialize kakao api

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import "./App.css";
 import {hot} from "react-hot-loader"
 import {Helmet} from 'react-helmet'
 
-
+  const JAVASCRIPT_KEY = "44d53cc36e8d332af1a95050ea17fafe";
+   if(window.Kakao){
+    window.Kakao.init(JAVASCRIPT_KEY);
+    if(window.Kakao.isInitialized())
+    {
+      alert('success')
+    }
+  }
+  
 function App(){
   
   
     return(
       <>
-          <Helmet>
-          <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-          </Helmet>
-            <button>
-              카카카오 공유
-            </button>
+        <Helmet>
+        <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+        </Helmet>
+      <button>
+        카카카오 공유
+      </button>
       </>
     );
   
