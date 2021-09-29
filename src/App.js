88ba1aa583f118792fa8d15794500967306e5308
k89jy <k89jy@naver.com>
2021-09-29 18:59:58 +0900

    feat:add react-helmet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component} from "react";
 import "./App.css";
 import {hot} from "react-hot-loader"
+import {Helmet} from 'react-helmet'
+
 
 function App(){
+  
+  
     return(
-      <button>
-        카카카오 공유
-      </button>
+      <>
+          <Helmet>
+          <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+          </Helmet>
+            <button>
+              카카카오 공유
+            </button>
+      </>
     );
   
 }
