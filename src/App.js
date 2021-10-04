b03cfbd887381c1b9d82ca5bc891eaf0a5da542d
k89jy <k89jy@naver.com>
2021-10-04 16:44:19 +0900

    initialize kakao api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import React, { Component} from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import {hot} from "react-hot-loader"
-import {Helmet} from 'react-helmet'
+import { hot } from "react-hot-loader";
+import { Helmet } from "react-helmet";
 
-  const JAVASCRIPT_KEY = "44d53cc36e8d332af1a95050ea17fafe";
-   if(window.Kakao){
-    window.Kakao.init(JAVASCRIPT_KEY);
-    if(window.Kakao.isInitialized())
-    {
-      alert('success')
+function App() {
+  useEffect(() => {
+    const JAVASCRIPT_KEY = process.env.REACT_APP_JAVASCRIPT_KEY;
+    alert(JAVASCRIPT_KEY);
+    if (window.Kakao) {
+      window.Kakao.init(JAVASCRIPT_KEY);
+      if (window.Kakao.isInitialized()) {
+        alert("success");
+      } else {
+        alert("kako not initialized");
+      }
+    } else {
+      alert("no kakao api");
     }
-  }
-  
-function App(){
-  
-  
-    return(
-      <>
-        <Helmet>
+  });
+
+  return (
+    <>
+      <Helmet>
         <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-        </Helmet>
-      <button>
-        카카카오 공유
-      </button>
-      </>
-    );
-  
+      </Helmet>
+      <button>카카카오 공유</button>
+    </>
+  );
 }
 
 export default hot(module)(App);
